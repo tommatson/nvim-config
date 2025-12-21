f65afbf45a0c19ee86c2575ac43f24ba033d4135
LopsidedMango <tomomatson@icloud.com>
2025-12-21 18:51:49 +0000

    Updated keybinds for undo and redo

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,3 +17,14 @@ vim.keymap.set('n', 'x', '"_x', { noremap = true, silent = true, desc = 'Delete 
 -- Line replacement when pasting
 vim.keymap.set('v', 'p', '"_dP', {noremap = true, silent = true, desc = 'Paste Replacement'})
 
+-- Undo with Ctrl+z in Normal mode
+vim.keymap.set('n', '<C-z>', 'u', { desc = 'Undo' })
+
+-- Undo with Ctrl+z in Insert mode (Go to Normal, undo, go back to Insert)
+vim.keymap.set('i', '<C-z>', '<C-o>u', { desc = 'Undo' })
+
+-- Redo in normal mode
+vim.keymap.set('n', '<C-g>', '<C-r>', { desc = 'Redo' })
+
+-- Redo in insert mode
+vim.keymap.set('i', '<C-g>', '<C-o><C-r>', { desc = 'Redo' })
