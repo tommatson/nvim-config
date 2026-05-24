4e823c7505755652d558bbfd2437d2392b6f1c99
LopsidedMango <tomomatson@icloud.com>
2026-05-24 21:42:07 +0100

    Add icons to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   branch = 'v3.x',
+  lazy = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
@@ -30,10 +31,10 @@ return {
           expander_expanded = '',
         },
         icon = {
-          folder_closed = '',
-          folder_open = '',
+          folder_closed = '',
+          folder_open = '',
           folder_empty = '󰜌',
-          default = '*',
+          default = '󰈚',
         },
         modified = {
           symbol = '●',
