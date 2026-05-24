307879502215e58108fa7dfdcf6e40dd1348f8b2
LopsidedMango <tomomatson@icloud.com>
2026-05-24 21:33:43 +0100

    Update keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,7 +21,14 @@ return {
         prefix = '●',
         spacing = 4,
       },
-      signs = true,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = ' ',
+          [vim.diagnostic.severity.WARN] = ' ',
+          [vim.diagnostic.severity.INFO] = ' ',
+          [vim.diagnostic.severity.HINT] = '󰌵 ',
+        },
+      },
       underline = true,
       update_in_insert = false,
       severity_sort = true,
@@ -32,13 +39,6 @@ return {
         prefix = ' ',
       },
     })
-
-    -- Diagnostic signs in the gutter
-    local signs = { Error = ' ', Warn = ' ', Hint = '󰌵 ', Info = ' ' }
-    for type, icon in pairs(signs) do
-      local hl = 'DiagnosticSign' .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-    end
 
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
